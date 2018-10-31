d2a69af8dd7a7a49b6aa5f1f4e1aec7d177342cf
nick77-s <44245934+nick77-s@users.noreply.github.com>
2018-10-31 14:34:37 -0500

    updated

--- 01-python/02-python/01-required/07-functions_intermediate_2.py
+++ 01-python/02-python/01-required/07-functions_intermediate_2.py
@@ -69,25 +69,3 @@ def dojoInfo(mydict):
     for i in range(len(mydict["instructors"])):
         print(mydict["instructors"][i])
 dojoInfo(dojo)
-
-arr = [1,3,5,7]
-print(arr)
-arr[0], arr[1] = arr[1], arr[0]
-print(arr)
-
-arr =[-4,8,19,-2,28,21]
- 
-min = 0    #set min = 0
-n = len(arr)
- 
-while(min <= n-1):
-  s_i = min
-  while(s_i <= n-1):      #finding minimum
-    if (arr[s_i] < arr[min]):
-      arr[min],arr[s_i] = arr[s_i],arr[min]   #swapping element at start index with minimum
-    s_i = s_i+1
- 
-  min = min+1
- 
-for element in arr:
-      print(element)
