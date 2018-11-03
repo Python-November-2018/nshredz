16fac360f52221635972d03a19ac4256a1eb6e11
nick77-s <44245934+nick77-s@users.noreply.github.com>
2018-11-03 15:46:14 -0500

    fixed randInt to reach 100, added test case to confirm

--- 01-python/02-python/01-required/06-functions_intermediate_1.py
+++ 01-python/02-python/01-required/06-functions_intermediate_1.py
@@ -1,12 +1,16 @@
 import random
 
 # randInt() returns a random integer between 0 to 100
-#def randInt():
-#    return (random.random()*100)
+x = 0
+y = 1
+def randInt():
+    return (random.random()*101)
 
-#x = int(randInt())
-#print(x)
-
+while (x != 100): # this is purely a test, see how many passes it takes to get to 100, the max possible int 
+        x = int(randInt())
+        print(x)
+        y += 1
+print(f"Total: {y}")
 # randInt(max=50) returns a random integer between 0 to 50
 #def randInt(max=50):
 #    return (random.random()*max)
@@ -23,11 +27,11 @@ import random
 #print(x)
 
 #randInt(min=50, max=500) returns a random integer between 50 and 500
-def randInt(min=50, max=500):
-    y = 0
-    while (y < min and y < max):
-        y = random.random()*max
-    return y
-for z in range(20):
-    x = int(randInt())
-    print(x) # afer looping a few times all appear to be between 50 and 500
+# def randInt(min=50, max=500):
+#     y = 0
+#     while (y < min and y < max):
+#         y = random.random()*max
+#     return y
+# for z in range(20):
+#     x = int(randInt())
+#     print(x) # afer looping a few times all appear to be between 50 and 500
