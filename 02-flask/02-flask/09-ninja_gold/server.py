571595f7ec9557b5b42e7d2adcd04e2ee2689674
nick77-s <44245934+nick77-s@users.noreply.github.com>
2018-11-15 18:44:54 -0600

    refactored to remove redundant code and build html in the template

--- 02-flask/02-flask/09-ninja_gold/server.py
+++ 02-flask/02-flask/09-ninja_gold/server.py
@@ -15,29 +15,48 @@ def index():
 
 @app.route('/process_money', methods=['POST'])
 def process_money():
-    if request.form['property'] == 'farm':
-        gold = random.randrange(10, 21)
-        session['total_gold'] += gold
-        result = '<p class="won">Earned ' + str(gold) + ' gold from the ' + request.form['property'] + '!    (' + datetime.now().strftime('%c') + ') </p>'
+    location_map = {
+        "farm": random.randrange(10, 21),
+        "cave": random.randrange(5, 11),
+        "house": random.randrange(2, 6),
+        "casino": random.randrange(-50, 51)
+    }
+
+    gold = location_map[request.form['property']]
+    session['total_gold'] += gold
+
+    activity = {}
+    if gold >= 0:
+        activity['css_class'] = "won"
+        activity['content'] = "Won {} gold from the {}! ({})".format(str(abs(gold)),  request.form['property'], datetime.now().strftime('%c'))
+    else:
+        activity['css_class'] = "lost"
+        activity['content'] = "Lost {} gold from the {}! ({})".format(str(abs(gold)),  request.form['property'], datetime.now().strftime('%c'))
+
+    # if request.form['property'] == 'farm':
+    #     gold = random.randrange(10, 21)
+    #     session['total_gold'] += gold
+    #     result = '<p class="won">Earned ' + str(gold) + ' gold from the ' + request.form['property'] + '!    (' + datetime.now().strftime('%c') + ') </p>'
     
-    elif request.form['property'] == 'cave':
-        gold = random.randrange(5, 11)
-        session['total_gold'] += gold
-        result = '<p class="won">Earned ' + str(gold) + ' gold from the ' + request.form['property'] + '!    (' + datetime.now().strftime('%c') + ') </p>'
+    # elif request.form['property'] == 'cave':
+    #     gold = random.randrange(5, 11)
+    #     session['total_gold'] += gold
+    #     result = '<p class="won">Earned ' + str(gold) + ' gold from the ' + request.form['property'] + '!    (' + datetime.now().strftime('%c') + ') </p>'
     
-    elif request.form['property'] == 'house':
-        gold = random.randrange(2, 6)
-        session['total_gold'] += gold
-        result = '<p class="won">Earned ' + str(gold) + ' gold from the ' + request.form['property'] + '!    (' + datetime.now().strftime('%c') + ') </p>'
+    # elif request.form['property'] == 'house':
+    #     gold = random.randrange(2, 6)
+    #     session['total_gold'] += gold
+    #     result = '<p class="won">Earned ' + str(gold) + ' gold from the ' + request.form['property'] + '!    (' + datetime.now().strftime('%c') + ') </p>'
     
-    elif request.form['property'] == 'casino':
-        gold = random.randrange(-50, 51)
-        session['total_gold'] += gold
-        if gold > -1:
-            result = '<p class="won">Earned ' + str(gold) + ' gold from the ' + request.form['property'] + '!    (' + datetime.now().strftime('%c') + ') </p>'
-        else:
-            result = '<p class="lost">Lost ' + str(abs(gold)) + ' gold from the ' + request.form['property'] + '!    (' + datetime.now().strftime('%c') + ') </p>'
-    session['activities'].append(result)
+    # elif request.form['property'] == 'casino':
+    #     gold = random.randrange(-50, 51)
+    #     session['total_gold'] += gold
+    #     if gold > -1:
+    #         result = '<p class="won">Earned ' + str(gold) + ' gold from the ' + request.form['property'] + '!    (' + datetime.now().strftime('%c') + ') </p>'
+    #     else:
+    #         # result = '<p class="lost">Lost ' + str(abs(gold)) + ' gold from the ' + request.form['property'] + '!    (' + datetime.now().strftime('%c') + ') </p>'
+    #         result = '<p class="lost">Lost {} gold from the {}! ({})</p>'.format(str(abs(gold)),  request.form['property'], datetime.now().strftime('%c'))
+    session['activities'].append(activity)
     return redirect("/")
 
 @app.route('/restart', methods=['POST'])
